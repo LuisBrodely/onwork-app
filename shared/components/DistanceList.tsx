c5eba0e331f7cb6186809c40c77dbafa57ac67a4
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2024-07-22 03:19:48 -0600

    changes

---
+++ shared/components/DistanceList.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, Pressable } from 'react-native';
+import Slider from '@react-native-community/slider';
+
+export const DistanceList = () => {
+  const [price, setPrice] = useState([10, 9999]);
+  const [distance, setDistance] = useState(100);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.section}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 12 }}>
+          <Text style={styles.title}>Distance</Text>
+          <Text style={styles.value}>{distance}km</Text>
+        </View>
+        <View style={styles.sliderContainer}>
+          <Slider
+            style={styles.slider}
+            minimumValue={0}
+            maximumValue={100}
+            minimumTrackTintColor="#EF3166"
+            maximumTrackTintColor="#C7C7CC"
+            thumbTintColor="#EF3166"
+            step={1}
+            value={distance}
+            onValueChange={value => setDistance(value)}
+          />
+        </View>
+        <Pressable
+          style={{ backgroundColor: '#EF3166', padding: 16, borderRadius: 8, alignItems: 'center', marginTop: 32 }}
+          onPress={() => console.log('View Results pressed range: ' + price)}
+        >
+          <Text style={{ color: '#fff' }}>
+            View Results
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+  section: {
+    marginBottom: 32,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  value: {
+    fontSize: 16,
+    color: '#EF3166',
+    marginBottom: 16,
+  },
+  sliderContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  slider: {
+    flex: 1,
+  },
+});
---
+++ shared/components/PriceList.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, Pressable } from 'react-native';
+import MultiSlider from '@ptomasroos/react-native-multi-slider';
+
+export const PriceList = () => {
+  const [price, setPrice] = useState([10, 9999]);
+
+  const handleValuesChange = (values: any) => {
+    setPrice(values);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 12 }}>
+          <Text style={styles.title}>Price</Text>
+          <Text style={styles.value}>${price[0]} to ${price[1]}</Text>
+        </View>
+        <View style={styles.sliderContainer}>
+          <MultiSlider
+            values={[price[0], price[1]]}
+            min={10}
+            max={9999}
+            step={1}
+            onValuesChange={handleValuesChange}
+            selectedStyle={styles.selectedTrack}
+            unselectedStyle={styles.unselectedTrack}
+            markerStyle={styles.marker}
+            containerStyle={styles.slider}
+            trackStyle={styles.track}
+          />
+        </View>
+        <Pressable
+          style={{ backgroundColor: '#EF3166', padding: 16, borderRadius: 8, alignItems: 'center', marginTop: 32 }}
+          onPress={() => console.log('View Results pressed range: ' + price)}
+        >
+          <Text style={{ color: '#fff' }}>
+            View Results
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    color: '#000',
+  },
+  value: {
+    fontSize: 16,
+    color: '#000',
+  },
+  sliderContainer: {
+    alignItems: 'center',
+  },
+  slider: {
+    height: 20,
+  },
+  track: {
+    height: 10,
+    borderRadius: 10,
+  },
+  selectedTrack: {
+    backgroundColor: '#EF3166',
+  },
+  unselectedTrack: {
+    backgroundColor: '#C7C7CC',
+  },
+  marker: {
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    backgroundColor: '#EF3166',
+  },
+});
